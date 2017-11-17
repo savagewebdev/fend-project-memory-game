b35726d92c936519b85c76e349e04a358a5331e3
savagewebdev <rbnsavage@gmail.com>
2017-11-16 21:25:10 -0600

    Working template

--- js/app.js
+++ js/app.js
@@ -1,7 +1,5 @@
 const cardsArray = ["fa fa-diamond", "fa fa-paper-plane-o", "fa fa-anchor", "fa fa-bolt", "fa fa-cube", "fa fa-anchor", "fa fa-leaf", "fa fa-bicycle", "fa fa-diamond", "fa fa-bomb", "fa fa-leaf", "fa fa-bomb", "fa fa-bolt", "fa fa-bicycle", "fa fa-paper-plane-o", "fa fa-cube"];
 
-const unorganizedList = $(".deck");
-
 function shuffle(cardsArray) { 
     var currentIndex = cardsArray.length, temporaryValue, randomIndex; 
 
@@ -16,36 +14,16 @@ function shuffle(cardsArray) {
     }
     return cardsArray;
 }
-
-    shuffle(cardsArray);
     
-const cardsTable = cardsArray.map(function callback(currentValue, index, array) {
-    const li = $(".deck").append("<li> </li>")
-        $("li").addClass("card")
-    const i = $(".deck li").append("<i> </i>")
-        $(".deck li i").addClass(currentValue)
-//    }  
+const cardsTable = shuffle(cardsArray).map(function callback(currentValue, index, array) {
+    const cardMe = "<li class='card'><i class=" + '"' + currentValue + '"' + "</i></li>"; 
+    $(".deck").append(cardMe);
 });
-
-//$.each(cardsArray, function(index, value) { // For each iteration of the array
-//    $(".card i").each(function() { // iterate through the cards
-//    });
-//    this.addClass(value); // do the magic
-//});
-
-
-
-//function randomize() {
-//    $(".restart").on('click', function(event) {
-//    	event.preventDefault(); 
-//        shuffle(cardsArray);
-//    });
-//    return randomize;
-//}
 
 $(".card").on('click', function() {  //event listener code
     $(event.target).addClass(event.target + " open show");
 });
+
 /*
 
 Pseudoclassical Template:
@@ -83,30 +61,5 @@ var amy = new Van(9);
 amy.move();
 
 */
-
-
-
-
-// Get something going that displays the cards within the memory game, so that the remaining portion can be effectively hacked.
-
-
-
-//function displayCards() { 
-//    const countDown = cardsArray.length;
-//    for (let i = 0; i < countDown; i++) { 
-//        let text.cardsArray[i];
-//    
-//    
-//    
-//     text = $(this).text ( );   
-//    
-//     $(this).text(text + “ “ + number); 
-//    
-//     $(‘p’).each(numberAdder);
-//    
-    
-
-        // do something that pertains to each iteration within (myArray[i]);
-//         $(".card).on;
 
 
