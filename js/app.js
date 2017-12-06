53df658af9e68b968f9901c5a617bc10f0f80e53
savagewebdev <rbnsavage@gmail.com>
2017-12-05 20:52:35 -0600

    Objectifying Test
    Attempting to display the game via Chrome.

--- js/app.js
+++ js/app.js
@@ -1,26 +1,26 @@
-const cardsArray = ["fa fa-diamond", "fa fa-paper-plane-o", "fa fa-anchor", "fa fa-bolt", "fa fa-cube", "fa fa-anchor", "fa fa-leaf", "fa fa-bicycle", "fa fa-diamond", "fa fa-bomb", "fa fa-leaf", "fa fa-bomb", "fa fa-bolt", "fa fa-bicycle", "fa fa-paper-plane-o", "fa fa-cube"];
-
-function shuffle(cardsArray) { 
-    var currentIndex = cardsArray.length, temporaryValue, randomIndex; 
-
-    while (currentIndex !== 0) { // While there remain elements left to shuffle...
-    
-        randomIndex = Math.floor(Math.random() * currentIndex); // Pick a remaining element
-        currentIndex -= 1;
-        
-        temporaryValue = cardsArray[currentIndex]; // And swap it with the current element
-        cardsArray[currentIndex] = cardsArray[randomIndex];
-        cardsArray[randomIndex] = temporaryValue;
-    }
-    return cardsArray;
-};
-
-shuffle(cardsArray).map(function callback(currentValue) { 
-    const addCard = "<li class='card'><i class=" + '"' + currentValue + '"' + "</i></li>"; 
-    $(".deck").append(addCard);
-});
-
 const myObect {
+    
+    cardsArray: ["fa fa-diamond", "fa fa-paper-plane-o", "fa fa-anchor", "fa fa-bolt", "fa fa-cube", "fa fa-anchor", "fa fa-leaf", "fa fa-bicycle", "fa fa-diamond", "fa fa-bomb", "fa fa-leaf", "fa fa-bomb", "fa fa-bolt", "fa fa-bicycle", "fa fa-paper-plane-o", "fa fa-cube"],
+    
+    shuffle: function(cardsArray) { 
+        var currentIndex = cardsArray.length, temporaryValue, randomIndex; 
+
+        while (currentIndex !== 0) { // While there remain elements left to shuffle...
+        
+            randomIndex = Math.floor(Math.random() * currentIndex); // Pick a remaining element
+            currentIndex -= 1;
+            
+            temporaryValue = cardsArray[currentIndex]; // And swap it with the current element
+            cardsArray[currentIndex] = cardsArray[randomIndex];
+            cardsArray[randomIndex] = temporaryValue;
+        }
+        return cardsArray;
+    },
+    
+    const myDisplay = myObject.shuffle.map(function callback(currentValue) { 
+        const addCard = "<li class='card'><i class=" + '"' + currentValue + '"' + "</i></li>"; 
+        $(".deck").append(addCard);
+    });
     
     selections: function (clicks) {
         myCard: $(".card"),
@@ -34,9 +34,11 @@ const myObect {
         .on ('click', function() {
             event.target.addClass(event.target) + " open show");
         ;
-    },
+    }
     
 };
+
+myObject[myDisplay](); 
 //
 //$(".card").on('click', function() { // When the card is clicked on...
     
