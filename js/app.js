f61eb0da0da1cb7da1348077febd3bd1341d742a
savagewebdev <rbnsavage@gmail.com>
2017-12-26 23:00:06 -0600

    Retrinkering

--- js/app.js
+++ js/app.js
@@ -15,66 +15,28 @@ function shuffle(cardsArray) {
     return cardsArray;
 }
     
-const shuffleMe = shuffle(cardsArray).map(function callback(currentValue) { 
+const ShuffleMe = shuffle(cardsArray).map(function callback(currentValue) { 
     const addCard = "<li class='card'><i class=" + '"' + currentValue + '"' + "</i></li>"; 
     $(".deck").append(addCard);
     });
 
-shuffleMe;
+ShuffleMe;
+    
+//const Cardobject = jQuery.event("click");
+//});
 
-const obj = {
-    
-    const Card = $
-    
-    Card.prototype.opened = function() {
-        
-    }
-    
-    const openedCards = [];
-    
-    
-};
-//    
-//    selections: function (clicks) {
-//        myCard: $(".card"),
-//        myRestart: $(".restart"),
-//        myScore: $(".stars"),
-//        myMoves: $(".moves")
-//    },
-//    
-//    cardMe: function () {
-//        myObject.selections.myCard
-//        .on ('click', function() {
-//            event.target.addClass(event.target) + " open show");
-//        ;
-//    }
-
-
-
-//
-//$(".card").on('click', function() { // When the card is clicked on...
-    
-//const cardOpen = {
-//    
-//    
-//}
-
-    
-    
-//const openCard = {
-    
-    //.on triggers function/method
-    //method triggers property edits 
-    
-
-
-//$(".card").on('click', function() { // When the card is clicked on...
+//Cardobject.prototype.flip = function() {
 //    $(event.target).addClass(event.target + " open show");
-//});
-//
-//$(".restart").on('click', function() {
-//    $(".deck").empty();
-//});
+//};
+
+
+
+
+
+const reStart = $(".restart").on('click', function() {
+    $(".deck").empty();
+    return ShuffleMe;
+});
 
 
 
