351740b04c3a7d967f1554f15801f44675e739e5
savagewebdev <rbnsavage@gmail.com>
2018-02-11 15:09:43 -0600

    Getting ready to objectify the array

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const Cards = ["fa fa-diamond", "fa fa-paper-plane-o", "fa fa-anchor", "fa fa-bolt", "fa fa-cube", "fa fa-anchor", "fa fa-leaf", "fa fa-bicycle", "fa fa-diamond", "fa fa-bomb", "fa fa-leaf", "fa fa-bomb", "fa fa-bolt", "fa fa-bicycle", "fa fa-paper-plane-o", "fa fa-cube"];
 
+
 Cards.prototype.shuffle = function (Cards) { 
     let currentIndex = Cards.length, temporaryValue, randomIndex; 
 
