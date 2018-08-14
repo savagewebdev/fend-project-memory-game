00a6ed7c7e8245db920d57baab2ad07caec15fc5
Robin Savage <macbookbro@Robins-MBP.domain>
2018-08-13 22:10:22 -0500

    Prototype of prototypes
    A prototype of revamped code.

--- js/Exmpl Code.js
+++ js/Exmpl Code.js
@@ -1,3 +1,37 @@
+function shuffle(array) { // Randomization of array
+	let currentIndex = array.length, temporaryValue, randomIndex; 
+
+	while (currentIndex !== 0) { // While there remain elements left to shuffle...
+	
+		randomIndex = Math.floor(Math.random() * currentIndex); // Pick a remaining element
+		currentIndex -= 1;
+		
+		temporaryValue = array[currentIndex]; // And swap it with the current element
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+	return array;
+}
+
+const cards = ["fa fa-diamond", "fa fa-paper-plane-o", "fa fa-anchor", "fa fa-bolt", "fa fa-cube", "fa fa-anchor", "fa fa-leaf", "fa fa-bicycle", "fa fa-diamond", "fa fa-bomb", "fa fa-leaf", "fa fa-bomb", "fa fa-bolt", "fa fa-bicycle", "fa fa-paper-plane-o", "fa fa-cube"]; 
+
+function Newcards() {
+	shuffle(cards).map(function callback (currentValue) { // Create a new shuffled array (map).
+	const addCard = "<li class='card'><i class=" + '"' + currentValue + '"' + "</i></li>"; 
+		$(".deck").append(addCard);    
+	});  
+}
+
+const openCards = []; // Empty array for the cards clicked upon.
+
+// Card selection and functionality
+
+const Card = function() {
+	$(".card").on('click', function(event) {
+		clickedCard(event.target);
+	});
+}
+
 function clickedCard(card) { // (card) = event.target
 	card.classList.add('open', 'show');
 	const i = card.firstChild;
@@ -14,7 +48,6 @@ function clickedCard(card) { // (card) = event.target
 		if (topCard.firstChild.className === i.className) { // to the card that was recently clicked.
 		Matches++;// it automatically remains open...  
 
-		
 		} else { // otherwise it closes within a certain timeframe.
 			setTimeout(() => {
 				topCard.classList.remove('open', 'show');
@@ -24,27 +57,83 @@ function clickedCard(card) { // (card) = event.target
 			}, 1 * 750);
 		}
 	}         
-	
-	if (Matches == 8) { // All the cards have been pushed to the open array...
-		clearInterval(Timer); // clear the timer
-		let txt;
-		let totalStars = $(".stars li").length;
-		let Message = confirm("You've won! It only took you " + moveCounter + " turns, " + Seconds + " seconds, and you earned a star rating of " + totalStars + ". Play again?");
-		if (Message == true) {
-			txt = "You pressed OK!";
-		} else {
-			txt = "You pressed Cancel!";
+		if (Matches == 8) { // All the cards have been pushed to the open array...
+			clearInterval(Timer); // clear the timer
+			let txt;
+			let totalStars = $(".stars li").length;
+			setTimeout(() => {
+				let Message = confirm("You've won! It only took you " + moveCounter + " turns, " + Seconds + " seconds, and you earned a star rating of " + totalStars + ". Play again?");
+				if (Message == true) {
+					txt = new Restart();
+				} else {
+					txt = "You pressed Cancel!";
+				}
+			}, 1 * 500);
 		}
-	}
-	else {
+		else {
+		}
+}
+
+// Moves
+
+const Moves = function(moveCounter) {
+	this.moveCounter = moveCounter;
+	if (moveCounter === 1) {
+		$(".moves").html("" + moveCounter + " Move");
+	} else {
+		$(".moves").html("" + moveCounter + " Moves");
 	}
 }
 
+Moves.prototype.add = function() { 
+	this.moveCounter++;
+}
 
+// Match
 
+const Match = function(Matches) { // A constructor function produces difference because of what passses through 
+	this.Matches = Matches; // to a new function.
+}
 
+Match.prototype.match = function() { // The prototype function is similar because, when used, it does the same to all.
+	this.Matches++;
+}
 
+// Stars
 
+const Stars = function(startingStars) {
+	this.startingStars = startingStars;
+	
+	for (var i = 0; i < startingStars; i++) {
+			$(".stars").append("<li><i class='fa fa-star'></i></li>");
+		}
+}
 
+Stars.prototype.star = function() {
+	startingStars--;
+	if (moveCounter == 11) {
+		$(".stars li:nth-child(1)").remove();
+	} else {
+	} if (moveCounter == 16) {
+		$(".stars li:nth-child(2)").remove();
+	}
+}
 
+// Timer
 
+const Timer = setInterval(Countdown, 1000); // timer logic
+	
+const Countdown = function(time) { // initializer of timer?
+	Seconds++;
+}
+
+Countdown.prototype.reset = function() { // stopper of timer
+	clearInterval(Timer);
+}
+
+// Reset Game
+
+const Restart = function() { // Moves and matches don't reset, stars go away.
+	$(".restart").on('click', function() {
+		$(".deck").empty();
+		$(".stars").empty();
